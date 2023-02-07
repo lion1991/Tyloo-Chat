2c471bca55a961e7b1916d939b1cd8b983606ee5
Lion_Matt <matt@wrlog.com>
2023-02-07 12:31:10 +0800

    Update global.ts

--- server/src/common/constant/global.ts
+++ server/src/common/constant/global.ts
@@ -6,10 +6,10 @@
  */
 
 export const defaultPassword = '123456' // 默认用户密码
-export const defaultRobot = '小冰机器人' // 机器人名称
+export const defaultRobot = '管理员' // 机器人名称
 export const defaultRobotId = 'robot' // 机器人ID
-export const defaultWelcomeMessage = '欢迎使用小冰机器人,有什么能帮您的呢?😃' // 机器人欢迎语
-export const defaultGroup = '系统问题反馈群' // 默认用户群组名称
+export const defaultWelcomeMessage = '忽略此消息😃' // 机器人欢迎语
+export const defaultGroup = '工程部总群' // 默认用户群组名称
 export const defaultGroupId = 'group' // 默认用户群组ID
 export const IMAGE_SAVE_PATH = 'public/static/image' // 聊天图片缓存路径
 export const FILE_SAVE_PATH = 'public/static/file' // 聊天附件缓存路径
